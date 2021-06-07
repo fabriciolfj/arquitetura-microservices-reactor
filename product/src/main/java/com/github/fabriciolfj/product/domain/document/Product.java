e862f3dba52c934a9e9aeb53842760fe75536013
fabricio <fabricio.jacob@outlook.com>
2021-06-07 18:59:27 -0300

    add

--- product/src/main/java/com/github/fabriciolfj/product/domain/document/Product.java
+++ product/src/main/java/com/github/fabriciolfj/product/domain/document/Product.java
@@ -1,10 +1,16 @@
 package com.github.fabriciolfj.product.domain.document;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Data
 @Document(collection = "products")
 public class Product {
